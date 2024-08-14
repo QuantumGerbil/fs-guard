75fffdb69caf008ace4b4ccc43a572baa22312cc
QuantumGerbil <69488134+QuantumGerbil@users.noreply.github.com>
2024-08-14 09:38:32 -0500

    Update sha256.rs
    moved bytes_to_hex to utility

--- src/sha256.rs
+++ src/sha256.rs
@@ -171,19 +171,6 @@ fn compress(hash_state: &mut [u32; 8], block: &[u8]) {
     hash_state[7] = hash_state[7].wrapping_add(h);
 }
 
-/// Converts a byte array to a hexadecimal string representation.
-///
-/// # Arguments
-///
-/// * `bytes` - A slice of bytes to be converted.
-///
-/// # Returns
-///
-/// A `String` containing the hexadecimal representation of the input bytes.
-pub fn bytes_to_hex(bytes: &[u8]) -> String {
-    bytes.iter().map(|byte| format!("{:02x}", byte)).collect()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
